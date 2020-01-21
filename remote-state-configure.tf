c28e727f3e838ad968579f686879f6a5027af364
sirspen <sirspen@gmail.com>
2020-01-21 15:19:50 +0000

    use s3 for state

--- remote-state-configure.tf
+++ remote-state-configure.tf
@@ -1,9 +1,15 @@
 terraform {
-  backend "remote" {
-    organization = "sirspen"
+  # backend "remote" {
+  #   organization = "sirspen"
+  #
+  #   workspaces {
+  #     name = "Example-Workspace"
+  #   }
+  # }
 
-    workspaces {
-      name = "Example-Workspace"
-    }
+  backend "s3" {
+    bucket = "test-spencer-terraform-remote-state"
+    key = "state/example.tfstate"
+    region = "eu-west-1"
   }
 }
